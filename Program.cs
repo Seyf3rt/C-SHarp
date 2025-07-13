06c05c434af1b83b74b430a5b4097b65eb37c043
Seyfert <147849753+Seyf3rt@users.noreply.github.com>
2025-07-13 14:31:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Diagnostics.Eventing.Reader;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace AppHost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int ataque = random.Next(1, 10);
+            int herohp = 10;
+            int monsterhp = 10;
+
+            while (monsterhp > 0 && herohp > 0)
+            {
+                ataque = random.Next(1, 10);
+                monsterhp -= ataque;
+                Console.WriteLine($"Monster was damaged and lost {ataque} health and now has {monsterhp} health.");
+                if (monsterhp > 0)
+                {
+                    ataque = random.Next(1, 10);
+                    herohp -= ataque;
+                    Console.WriteLine($"Hero was damaged and lost {ataque} health and now has {herohp} health.");
+                }
+
+                if (monsterhp <= 0)
+                    Console.WriteLine("Hero wins!!");
+                if (herohp <= 0)
+                    Console.WriteLine("Monster wins!!");
+
+            }
+        }
+    }
+}
